bbf3b79e32a7f66dbc678a763b61d5b170162ee6
Nik002 <cyanikil002@gmail.com>
2022-07-25 22:01:07 +0530

    Edited font style

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,24 +14,28 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          titleTextStyle: const TextStyle(height: 2.0),
           elevation: 0.0,
           backgroundColor: Colors.white,
           title: const Text(
             "Events",
-            style: TextStyle(color: Colors.black),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500),
           ),
           centerTitle: true,
           bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(kToolbarHeight),
+            preferredSize: const Size.fromHeight(30.0),
             child: Container(
                 padding: const EdgeInsets.all(2.0),
                 margin: const EdgeInsets.all(10.0),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(8),
                   color: Colors.grey[300],
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.all(1.0),
                   child: Material(
                     color: Colors.grey[300],
                     child: Row(
@@ -44,9 +48,10 @@ class _HomepageState extends State<Homepage> {
                             decoration: const InputDecoration.collapsed(
                               hintText: ' Search',
                               hintStyle: TextStyle(
-                                  fontFamily: 'Roboto',
-                                  fontWeight: FontWeight.w100,
-                                  color: Colors.black),
+                                  fontFamily: 'Poppins',
+                                  fontSize: 13.0,
+                                  // fontWeight: FontWeight.w100,
+                                  color: Colors.black45),
                             ),
                             onChanged: (value) {},
                           ),
@@ -58,7 +63,7 @@ class _HomepageState extends State<Homepage> {
           ),
         ),
         body: ListView.builder(
-          itemCount: 6,
+          itemCount: 3,
           itemBuilder: (context, index) {
             return const EventBanner();
           },
